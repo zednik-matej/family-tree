8a9603661d32122bf7f9815388607a807a2b5288
zednik-matej <zednik.mattej@gmail.com>
2023-04-18 12:22:03 +0200

    First commit

---
+++ scripts/load-file.js
@@ -0,0 +1,19 @@
+function readSingleFile(e) {
+    var file = e.target.files[0];
+    if (!file) {
+      return;
+    }
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        var content = e.target.result;
+        var data = parse(content);
+        store();
+        //file-load
+        var element = document.getElementById('file-load');
+        element.textContent = 'File loaded';
+    };
+    reader.readAsText(file);
+  }
+
+document.getElementById('file-input')
+    .addEventListener('change', readSingleFile, false);
